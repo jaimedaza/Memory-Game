192d1b4d87a888d122bba0e9f929dbfed6125e0b
Jaime Daza <jaimedaza11@gmail.com>
2018-02-15 10:34:35 -0500

    Create the inital card deck

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
  * Create a list that holds all of your cards
  */
 
- const cards = [];
+ const cards = ["card1", "card2", "card3", "card4", "card5", "card6", "card7", "card8", "card9", "card10", "card11", "card12", "card13", "card14", "card15", "card16"];
 
 
 /*
@@ -11,6 +11,82 @@
  *   - loop through each card and create its HTML
  *   - add each card's HTML to the page
  */
+
+ // Shuffles the cards using the provided "shuffle" function
+ document.addEventListener('DOMContentLoaded', shuffle(cards));
+
+//Loops through each card and create its HTML and adds each card's HTML to the page
+ const deck = document.querySelector('.deck');
+
+ for(i = 0; i < cards.length; i++){
+ 	if(cards[i]==='card1'){
+ 		const card = '<li class="card"><i class="fa fa-diamond"></i></li>';
+ 	    deck.insertAdjacentHTML('beforeend', card);
+ 	}
+ 	else if(cards[i]==='card2'){
+ 		const card = '<li class="card"><i class="fa fa-diamond"></i></li>';
+ 	    deck.insertAdjacentHTML('beforeend', card);
+ 	}
+ 	else if(cards[i]==='card3'){
+ 		const card = '<li class="card"><i class="fa fa-paper-plane-o"></i></li>';
+ 	    deck.insertAdjacentHTML('beforeend', card);
+ 	}
+ 	else if(cards[i]==='card4'){
+ 		const card = '<li class="card"><i class="fa fa-paper-plane-o"></i></li>';
+ 	    deck.insertAdjacentHTML('beforeend', card);
+ 	}
+ 	else if(cards[i]==='card5'){
+ 		const card = '<li class="card"><i class="fa fa-anchor"></i></li>';
+ 	    deck.insertAdjacentHTML('beforeend', card);
+ 	}
+ 	else if(cards[i]==='card6'){
+ 		const card = '<li class="card"><i class="fa fa-anchor"></i></li>';
+ 	    deck.insertAdjacentHTML('beforeend', card);
+ 	}
+ 	else if(cards[i]==='card7'){
+ 		const card = '<li class="card"><i class="fa fa-bolt"></i></li>';
+ 	    deck.insertAdjacentHTML('beforeend', card);
+ 	}
+ 	else if(cards[i]==='card8'){
+ 		const card = '<li class="card"><i class="fa fa-bolt"></i></li>';
+ 	    deck.insertAdjacentHTML('beforeend', card);
+ 	}
+ 	else if(cards[i]==='card9'){
+ 		const card = '<li class="card"><i class="fa fa-cube"></i></li>';
+ 	    deck.insertAdjacentHTML('beforeend', card);
+ 	}
+ 	else if(cards[i]==='card10'){
+ 		const card = '<li class="card"><i class="fa fa-cube"></i></li>';
+ 	    deck.insertAdjacentHTML('beforeend', card);
+ 	}
+ 	else if(cards[i]==='card11'){
+ 		const card = '<li class="card"><i class="fa fa-leaf"></i></li>';
+ 	    deck.insertAdjacentHTML('beforeend', card);
+ 	}
+ 	else if(cards[i]==='card12'){
+ 		const card = '<li class="card"><i class="fa fa-leaf"></i></li>';
+ 	    deck.insertAdjacentHTML('beforeend', card);
+ 	}
+ 	else if(cards[i]==='card13'){
+ 		const card = '<li class="card"><i class="fa fa-bicycle"></i></li>';
+ 	    deck.insertAdjacentHTML('beforeend', card);
+ 	}
+ 	else if(cards[i]==='card14'){
+ 		const card = '<li class="card"><i class="fa fa-bicycle"></i></li>';
+ 	    deck.insertAdjacentHTML('beforeend', card);
+ 	}
+ 	else if(cards[i]==='card15'){
+ 		const card = '<li class="card"><i class="fa fa-bomb"></i></li>';
+ 	    deck.insertAdjacentHTML('beforeend', card);
+ 	}
+ 	else{
+ 		const card = '<li class="card"><i class="fa fa-bomb"></i></li>';
+ 	    deck.insertAdjacentHTML('beforeend', card);
+ 	}
+ 	 	
+ };
+
+ //FUNCTIONS
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -39,6 +115,9 @@ function shuffle(array) {
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
 
+for(i = 0; i < cards.length; i++){
+
+}
  const selectCard = document.querySelector('.card');
  selectCard.addEventListener('click', function(){
  	cardSymbol(selectCard);
@@ -48,7 +127,14 @@ function shuffle(array) {
 // Function that flips the card
 
  function cardSymbol(card) {
- 	const listOfClasses = card.classList;
- 	listOfClasses.toggle('match');
- 	console.log(listOfClasses);
+ 	//const listOfClasses = card.classList;
+ 	card.classList.add('open', 'show');
+ 	//console.log(listOfClasses);
  };
+
+
+ //Restart the game
+ const restartButton = document.querySelector('.restart');
+ restartButton.addEventListener('click', function(){
+ 	location.reload();
+ });
