2003104b7c56308635a1b1f89b942965f425c401
Jaime Daza <jaimedaza11@gmail.com>
2018-03-08 17:45:11 -0500

    Final version

--- README.md
+++ README.md
@@ -3,18 +3,9 @@
 ## Table of Contents
 
 * [Instructions](#instructions)
-* [Contributing](#contributing)
 
 ## Instructions
 
-The starter project has some HTML and CSS styling to display a static version of the Memory Game project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
+Cheallenge yourself to memorize cards in a grid of 16 pairs. Click on a card to flip it over. Click a second card to attempt to make a match. To win the game you must reveal all of the cards by correctly matching pairs.
 
-To get started, open `js/app.js` and start building out the app's functionality
-
-For specific, detailed instructions, look at the project instructions in the [Udacity Classroom](https://classroom.udacity.com/me).
-
-## Contributing
-
-This repository is the starter code for _all_ Udacity students. Therefore, we most likely will not accept pull requests.
-
-For details, check out [CONTRIBUTING.md](CONTRIBUTING.md).
+As your number of moves increases, your star rating will decrease at set intervals. Beating the game in fewer moves will require concentration and memorization.
--- js/app.js
+++ js/app.js
@@ -2,7 +2,24 @@
  * Create a list that holds all of your cards
  */
 
-const cards = ["card1", "card2", "card3", "card4", "card5", "card6", "card7", "card8", "card9", "card10", "card11", "card12", "card13", "card14", "card15", "card16"];
+const cards = [
+  "fa-diamond",
+  "fa-diamond",
+  "fa-anchor",
+  "fa-anchor",
+  "fa-bolt",
+  "fa-bolt",
+  "fa-cube",
+  "fa-cube",
+  "fa-leaf",
+  "fa-leaf",
+  "fa-bicycle",
+  "fa-bicycle",
+  "fa-bomb",
+  "fa-bomb",
+  "fa-paper-plane-o",
+  "fa-paper-plane-o"
+];
 
 // List for holding open cards
 
@@ -27,356 +44,48 @@ let starRating = 3;
 // Creates the timer - timer from https://github.com/albert-gonzalez/easytimer.js/
 
 var timer = new Timer();
-timer.addEventListener('secondsUpdated', function (e) {
-	document.querySelector('#basicUsage').innerHTML = timer.getTimeValues().toString();    
+timer.addEventListener("secondsUpdated", function(e) {
+  document.querySelector(
+    "#basicUsage"
+  ).innerHTML = timer.getTimeValues().toString();
 });
 
- // Shuffles the cards using the provided "shuffle" function
+// Shuffles the cards using the provided "shuffle" function
 
- document.addEventListener('DOMContentLoaded', shuffle(cards));
+document.addEventListener("DOMContentLoaded", shuffle(cards));
+
+//FUNCTION TO CREATE CARDS
+
+const deck = document.querySelector(".deck");
+
+function createCard(x) {
+  const cardLi = document.createElement("li");
+  const cardI = document.createElement("i");
+  cardLi.classList.add("card");
+  cardI.classList.add("fa");
+  cardI.classList.add(x);
+  cardLi.appendChild(cardI);
+  deck.appendChild(cardLi);
+  cardLi.addEventListener("click", function(event) {
+    cardClick();
+    if (openCards.length > 0 && cardClicked === openCards[0]) {
+      cardClick();
+    } else {
+      showCard(cardLi);
+      if (openCards.length === 2) {
+        checkCard();
+      } else if (openCards.length > 2) {
+        checkCard2();
+      }
+    }
+  });
+}
 
 //Loops through each card and create its HTML and adds each card's HTML to the page
 
- const deck = document.querySelector('.deck');
-
- for(i = 0; i < cards.length; i++){
- 	if(cards[i]==='card1'){
- 		const cardLi = document.createElement('li');
- 		const cardI = document.createElement('i'); 		
-		cardLi.classList.add('card');
-		cardI.classList.add('fa');
-		cardI.classList.add('fa-diamond');		
-		cardLi.appendChild(cardI);
-		deck.appendChild(cardLi);
-		cardLi.addEventListener('click', function(event){
-			cardClick();
-			if(openCards.length > 0 && cardClicked === openCards[0]){
-				cardClick();
-			}
-			else{
-				showCard(cardLi);
- 			    if(openCards.length === 2){
- 				checkCard();
- 			    }
-			} 			
- 		}); 		
- 	}
- 	else if(cards[i]==='card2'){
- 		const cardLi = document.createElement('li');
- 		const cardI = document.createElement('i'); 		
-		cardLi.classList.add('card');
-		cardI.classList.add('fa');
-		cardI.classList.add('fa-diamond');		
-		cardLi.appendChild(cardI);
-		deck.appendChild(cardLi);
-		cardLi.addEventListener('click', function(event){
-			cardClick();
-			if(openCards.length > 0 && cardClicked === openCards[0]){
-				cardClick();
-			}
-			else{
-				showCard(cardLi);
- 			    if(openCards.length === 2){
- 				checkCard();
- 			    }
-			} 			
- 		}); 		
- 	}
- 	else if(cards[i]==='card3'){
- 		const cardLi = document.createElement('li');
- 		const cardI = document.createElement('i'); 		
-		cardLi.classList.add('card');
-		cardI.classList.add('fa');
-		cardI.classList.add('fa-paper-plane-o');		
-		cardLi.appendChild(cardI);
-		deck.appendChild(cardLi);
-		cardLi.addEventListener('click', function(event){
-			cardClick();
-			if(openCards.length > 0 && cardClicked === openCards[0]){
-				cardClick();
-			}
-			else{
-				showCard(cardLi);
- 			    if(openCards.length === 2){
- 				checkCard();
- 			    }
-			} 	
- 		}); 		
- 	}
- 	else if(cards[i]==='card4'){
- 		const cardLi = document.createElement('li');
- 		const cardI = document.createElement('i'); 		
-		cardLi.classList.add('card');
-		cardI.classList.add('fa');
-		cardI.classList.add('fa-paper-plane-o');		
-		cardLi.appendChild(cardI);
-		deck.appendChild(cardLi);
-		cardLi.addEventListener('click', function(event){
-			cardClick();
-			if(openCards.length > 0 && cardClicked === openCards[0]){
-				cardClick();
-			}
-			else{
-				showCard(cardLi);
- 			    if(openCards.length === 2){
- 				checkCard();
- 			    }
-			} 	
- 		}); 		
- 	}
- 	else if(cards[i]==='card5'){
- 		const cardLi = document.createElement('li');
- 		const cardI = document.createElement('i'); 		
-		cardLi.classList.add('card');
-		cardI.classList.add('fa');
-		cardI.classList.add('fa-anchor');		
-		cardLi.appendChild(cardI);
-		deck.appendChild(cardLi);
-		cardLi.addEventListener('click', function(event){
-			cardClick();
-			if(openCards.length > 0 && cardClicked === openCards[0]){
-				cardClick();
-			}
-			else{
-				showCard(cardLi);
- 			    if(openCards.length === 2){
- 				checkCard();
- 			    }
-			} 	
- 		});
- 	}
- 	else if(cards[i]==='card6'){
- 		const cardLi = document.createElement('li');
- 		const cardI = document.createElement('i'); 		
-		cardLi.classList.add('card');
-		cardI.classList.add('fa');
-		cardI.classList.add('fa-anchor');		
-		cardLi.appendChild(cardI);
-		deck.appendChild(cardLi);
-		cardLi.addEventListener('click', function(event){
-			cardClick();
-			if(openCards.length > 0 && cardClicked === openCards[0]){
-				cardClick();
-			}
-			else{
-				showCard(cardLi);
- 			    if(openCards.length === 2){
- 				checkCard();
- 			    }
-			} 	
- 		});
- 	}
- 	else if(cards[i]==='card7'){
- 		const cardLi = document.createElement('li');
- 		const cardI = document.createElement('i'); 		
-		cardLi.classList.add('card');
-		cardI.classList.add('fa');
-		cardI.classList.add('fa-bolt');		
-		cardLi.appendChild(cardI);
-		deck.appendChild(cardLi);
-		cardLi.addEventListener('click', function(event){
-			cardClick();
-			if(openCards.length > 0 && cardClicked === openCards[0]){
-				cardClick();
-			}
-			else{
-				showCard(cardLi);
- 			    if(openCards.length === 2){
- 				checkCard();
- 			    }
-			} 	
- 		});
- 	}
- 	else if(cards[i]==='card8'){
- 		const cardLi = document.createElement('li');
- 		const cardI = document.createElement('i'); 		
-		cardLi.classList.add('card');
-		cardI.classList.add('fa');
-		cardI.classList.add('fa-bolt');		
-		cardLi.appendChild(cardI);
-		deck.appendChild(cardLi);
-		cardLi.addEventListener('click', function(event){
-			cardClick();
-			if(openCards.length > 0 && cardClicked === openCards[0]){
-				cardClick();
-			}
-			else{
-				showCard(cardLi);
- 			    if(openCards.length === 2){
- 				checkCard();
- 			    }
-			} 	
- 		});
- 	}
- 	else if(cards[i]==='card9'){
- 		const cardLi = document.createElement('li');
- 		const cardI = document.createElement('i'); 		
-		cardLi.classList.add('card');
-		cardI.classList.add('fa');
-		cardI.classList.add('fa-cube');		
-		cardLi.appendChild(cardI);
-		deck.appendChild(cardLi);
-		cardLi.addEventListener('click', function(event){
-			cardClick();
-			if(openCards.length > 0 && cardClicked === openCards[0]){
-				cardClick();
-			}
-			else{
-				showCard(cardLi);
- 			    if(openCards.length === 2){
- 				checkCard();
- 			    }
-			} 	
- 		});
- 	}
- 	else if(cards[i]==='card10'){
- 		const cardLi = document.createElement('li');
- 		const cardI = document.createElement('i'); 		
-		cardLi.classList.add('card');
-		cardI.classList.add('fa');
-		cardI.classList.add('fa-cube');		
-		cardLi.appendChild(cardI);
-		deck.appendChild(cardLi);
-		cardLi.addEventListener('click', function(event){
-			cardClick();
-			if(openCards.length > 0 && cardClicked === openCards[0]){
-				cardClick();
-			}
-			else{
-				showCard(cardLi);
- 			    if(openCards.length === 2){
- 				checkCard();
- 			    }
-			} 	
- 		});
- 	}
- 	else if(cards[i]==='card11'){
- 		const cardLi = document.createElement('li');
- 		const cardI = document.createElement('i'); 		
-		cardLi.classList.add('card');
-		cardI.classList.add('fa');
-		cardI.classList.add('fa-leaf');		
-		cardLi.appendChild(cardI);
-		deck.appendChild(cardLi);
-		cardLi.addEventListener('click', function(event){
-			cardClick();
-			if(openCards.length > 0 && cardClicked === openCards[0]){
-				cardClick();
-			}
-			else{
-				showCard(cardLi);
- 			    if(openCards.length === 2){
- 				checkCard();
- 			    }
-			} 	
- 		});
- 	}
- 	else if(cards[i]==='card12'){
- 		const cardLi = document.createElement('li');
- 		const cardI = document.createElement('i'); 		
-		cardLi.classList.add('card');
-		cardI.classList.add('fa');
-		cardI.classList.add('fa-leaf');		
-		cardLi.appendChild(cardI);
-		deck.appendChild(cardLi);
-		cardLi.addEventListener('click', function(event){
-			cardClick();
-			if(openCards.length > 0 && cardClicked === openCards[0]){
-				cardClick();
-			}
-			else{
-				showCard(cardLi);
- 			    if(openCards.length === 2){
- 				checkCard();
- 			    }
-			} 	
- 		});
- 	}
- 	else if(cards[i]==='card13'){
- 		const cardLi = document.createElement('li');
- 		const cardI = document.createElement('i'); 		
-		cardLi.classList.add('card');
-		cardI.classList.add('fa');
-		cardI.classList.add('fa-bicycle');		
-		cardLi.appendChild(cardI);
-		deck.appendChild(cardLi);
-		cardLi.addEventListener('click', function(event){
-			cardClick();
-			if(openCards.length > 0 && cardClicked === openCards[0]){
-				cardClick();
-			}
-			else{
-				showCard(cardLi);
- 			    if(openCards.length === 2){
- 				checkCard();
- 			    }
-			} 				
- 		});
- 	}
- 	else if(cards[i]==='card14'){
- 		const cardLi = document.createElement('li');
- 		const cardI = document.createElement('i'); 		
-		cardLi.classList.add('card');
-		cardI.classList.add('fa');
-		cardI.classList.add('fa-bicycle');		
-		cardLi.appendChild(cardI);
-		deck.appendChild(cardLi);
-		cardLi.addEventListener('click', function(event){
-			cardClick();
-			if(openCards.length > 0 && cardClicked === openCards[0]){
-				cardClick();
-			}
-			else{
-				showCard(cardLi);
- 			    if(openCards.length === 2){
- 				checkCard();
- 			    }
-			} 			 	
- 		});
- 	}
- 	else if(cards[i]==='card15'){
- 		const cardLi = document.createElement('li');
- 		const cardI = document.createElement('i'); 		
-		cardLi.classList.add('card');
-		cardI.classList.add('fa');
-		cardI.classList.add('fa-bomb');		
-		cardLi.appendChild(cardI);
-		deck.appendChild(cardLi);
-		cardLi.addEventListener('click', function(event){
-			cardClick();
-			if (openCards.length > 0 && cardClicked === openCards[0]){
-				cardClick();
-			}
-			else{
-				showCard(cardLi);
-				if(openCards.length === 2){
-					checkCard();
-				}
-			} 				
- 		}); 		
- 	}
- 	else{
- 		const cardLi = document.createElement('li');
- 		const cardI = document.createElement('i'); 		
-		cardLi.classList.add('card');
-		cardI.classList.add('fa');
-		cardI.classList.add('fa-bomb');		
-		cardLi.appendChild(cardI);
-		deck.appendChild(cardLi);
-		cardLi.addEventListener('click', function(event){
-		    cardClick();			
-			if(openCards.length > 0 && cardClicked === openCards[0]){
-				cardClick();
-			}
-			else{				
-				showCard(cardLi);
- 			    if(openCards.length === 2){
- 				checkCard();
- 			  }
-			}			
- 	    }); 	 	
-    }
-};
+for (i = 0; i < cards.length; i++) {
+  createCard(cards[i]);
+}
 
 /*
  * set up the event listener for a card. If a card is clicked:
@@ -389,143 +98,171 @@ timer.addEventListener('secondsUpdated', function (e) {
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
 
- //FUNCTIONS
+//FUNCTIONS
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+  var currentIndex = array.length,
+    temporaryValue,
+    randomIndex;
 
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
 
-    return array;
+  return array;
 }
 
 // Function that flips the card
 
- function showCard(card) {
- 	timer.start();
- 	if(openCards.length === 0){
- 		card.classList.add('open', 'show'); 		
- 		openCards.push(card);
- 			
- 	}
- 	else if(openCards.length === 1){
- 		card.classList.add('open', 'show'); 		
- 		openCards.push(card); 		
- 	} 	
- };
+function showCard(card) {
+  timer.start();
+  if (openCards.length === 0) {
+    card.classList.add("open", "show");
+    openCards.push(card);
+  } else if (openCards.length === 1) {
+    card.classList.add("open", "show");
+    openCards.push(card);
+  } else {
+    openCards.push(card);
+  }
+}
 
- // Function to check the cards
+// Function to check the cards
 
- function checkCard(){ 	
- 	if(openCards[0].firstChild.className == openCards[1].firstChild.className){
- 		//openCards.push(card);
- 		openCards[0].classList.add('match');
- 		openCards[1].classList.add('match');
- 		openCards[0].classList.remove('open','show');
- 		openCards[1].classList.remove('open', 'show');
- 		removeOpenCards();
- 		updateMoves();
- 		checkWin();
- 	}
- 	else {
- 		openCards[0].classList.add('noMatch');
- 		openCards[1].classList.add('noMatch');
- 		setTimeout(hideCards, 1000);
- 		updateMoves(); 	    
- 	}
- };
+function checkCard() {
+  if (openCards[0].firstChild.className === openCards[1].firstChild.className) {
+    //openCards.push(card);
+    openCards[0].classList.add("match");
+    openCards[1].classList.add("match");
+    openCards[0].classList.remove("open", "show");
+    openCards[1].classList.remove("open", "show");
+    removeOpenCards();
+    updateMoves();
+    checkWin();
+  } else if (
+    openCards[0].firstChild.className !== openCards[1].firstChild.className
+  ) {
+    openCards[0].classList.add("noMatch");
+    openCards[1].classList.add("noMatch");
+    setTimeout(hideCards, 800);
+    updateMoves();
+  } else {
+    return;
+  }
+}
 
- //Function to hide no matched cards
+// Function to prevent an increase in the move count when clicking a third card while there are two open cards on the board
 
- function hideCards(){ 		
- 		openCards[0].classList.remove('open', 'show');
- 		openCards[1].classList.remove('open', 'show');
- 		openCards[0].classList.remove('noMatch');
- 		openCards[1].classList.remove('noMatch');
- 		removeOpenCards();
- }
+function checkCard2() {
+  openCards[0].classList.add("noMatch");
+  openCards[1].classList.add("noMatch");
+  setTimeout(hideCards2, 800);
+}
 
- //Function to clear openCards Array
+//Function to hide no matched cards
 
- function removeOpenCards(){
- 	openCards = [];
- }
+function hideCards() {
+  if (openCards.length === 2) {
+    openCards[0].classList.remove("open", "show");
+    openCards[1].classList.remove("open", "show");
+    openCards[0].classList.remove("noMatch");
+    openCards[1].classList.remove("noMatch");
+    removeOpenCards();
+  } else {
+    return;
+  }
+}
 
- // Function to increase the move count
+// function to hide and remove cards when there are unopened cards in the Opencards array
 
- function updateMoves(){
- 	moves += 1;
- 	const move = document.querySelector('.moves');
- 	move.innerHTML = moves;
- 	if(moves === 16){
- 		removeStar();
- 		starRating = 2;
- 	}
- 	else if(moves === 25){
- 		removeStar();
- 		starRating = 1;
- 	}
- }
+function hideCards2() {
+  if (openCards.length > 2) {
+    openCards[0].classList.remove("open", "show");
+    openCards[1].classList.remove("open", "show");
+    openCards[0].classList.remove("noMatch");
+    openCards[1].classList.remove("noMatch");
+    removeOpenCards();
+  } else {
+    return;
+  }
+}
+
+//Function to clear openCards Array
+
+function removeOpenCards() {
+  openCards = [];
+}
+
+// Function to increase the move count
+
+function updateMoves() {
+  moves += 1;
+  const move = document.querySelector(".moves");
+  move.innerHTML = moves;
+  if (moves === 16) {
+    removeStar();
+    starRating = 2;
+  } else if (moves === 25) {
+    removeStar();
+    starRating = 1;
+  }
+}
 
 // Function used to remove stars from the star rating
 
- function removeStar(){
- 	const star = document.querySelector('.stars');
- 	const starChild = star.firstElementChild;
- 	star.removeChild(starChild);
- }
-
+function removeStar() {
+  const star = document.querySelector(".stars");
+  const starChild = star.firstElementChild;
+  star.removeChild(starChild);
+}
 
 // Function to check if the player has won
 
 function checkWin() {
-    matchFound += 1;
-    if (matchFound == 8) {
-    	const message = document.getElementById('winnerPopup');
-		const totalMove = document.getElementById('totalMoves');
-		const totalStar = document.getElementById('totalStars')
-		const time = document.getElementById('totalTime');
-		const playAgain = document.getElementById('playAgainButton');
-		totalMove.innerHTML = moves;
-		totalTime.innerHTML = timer.getTimeValues().toString();
-		totalStar.innerHTML = starRating;
-		playAgain.addEventListener('click', function(){location.reload();});
-		message.style.display = 'block';
-		timer.pause();        
-    }
+  matchFound += 1;
+  if (matchFound == 8) {
+    const message = document.getElementById("winnerPopup");
+    const totalMove = document.getElementById("totalMoves");
+    const totalStar = document.getElementById("totalStars");
+    const time = document.getElementById("totalTime");
+    const playAgain = document.getElementById("playAgainButton");
+    totalMove.innerHTML = moves;
+    totalTime.innerHTML = timer.getTimeValues().toString();
+    totalStar.innerHTML = starRating;
+    playAgain.addEventListener("click", function() {
+      location.reload();
+    });
+    message.style.display = "block";
+    timer.pause();
+  }
 }
 
 // Function for the cards click events
 
- function cardClick(){ 	
-   if(event.target.nodeName == "LI"){ // Card area is clicked
-     cardClicked = event.target;
-    } else if(event.target.nodeName == "I"){ // card icon is clicked
-        cardClicked = event.target.parentElement;
-    } else {
-        return; // Empty deck area clicked
-    }
-    if(cardClicked === openCards[0]){              
-      return;
-    }
+function cardClick() {
+  if (event.target.nodeName == "LI") {
+    // Card area is clicked
+    cardClicked = event.target;
+  } else if (event.target.nodeName == "I") {
+    // card icon is clicked
+    cardClicked = event.target.parentElement;
+  } else {
+    return; // Empty deck area clicked
+  }
+  if (cardClicked === openCards[0]) {
+    return;
+  }
 }
 
+//Restart the game
 
- //Restart the game
-
- const restartButton = document.querySelector('.restart');
- restartButton.addEventListener('click', function(){
- 	location.reload();
- });
-
-
-
- 
+const restartButton = document.querySelector(".restart");
+restartButton.addEventListener("click", function() {
+  location.reload();
+});
